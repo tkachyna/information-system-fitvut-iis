04ee8ecd14e7a978cf46b53249d95d83f8fe9ac2
Lada Krofingerová <xkrofi00@stud.fit.vutbr.cz>
2022-10-23 21:42:23 +0200

    Basic DB
    Created a database structure, tables, columns, relations (hopefully correctly)... Also created a django app in the project called "app" to test migration and the creation of the DB, could be (and propably will be) removed later... Checks of the special conditions like phone number etc or change to real db (for now it's SQLlite) is TODO, this is now for development purposes

---
+++ smart_city/app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Person, Ticket, Request, TicketComment, RequestComment, Picture
+# Register your models here.
+
+admin.site.register(Person)
+admin.site.register(Ticket)
+admin.site.register(Request)
+admin.site.register(TicketComment)
+admin.site.register(RequestComment)
+admin.site.register(Picture)
+
