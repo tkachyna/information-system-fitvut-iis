cc62e9855cb609744a4661bd18f85ee9da2c7d19
AveAvatar <t.kachyna@email.cz>
2022-11-18 20:30:24 +0100

    settings updated

--- isystem/settings.py
+++ isystem/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(8kvo!*ns@-2%_xf05duf!a^n0437b931s=$v%t=q-19-lgu=o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['smart-city-test.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
