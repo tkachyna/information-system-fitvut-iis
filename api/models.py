07369599ac2798ba14d6ee421cf5ca7dd1208f03
Lada Krofingerová <xkrofi00@stud.fit.vutbr.cz>
2022-11-20 18:34:55 +0100

    Database update

--- api/models.py
+++ api/models.py
@@ -5,6 +5,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
+from django.core.validators import RegexValidator
+from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -18,9 +20,9 @@ class User(AbstractUser):
     role = models.PositiveSmallIntegerField(choices=USER_ROLE_CHOICES, default=1)
     city = models.CharField(max_length=256)
     street = models.CharField(max_length=256)
-    house_number = models.CharField(max_length=256)  # regex??
-    zipcode = models.CharField(max_length=6)  # regex: 3 nums, space, 2 nums
-    phone_number = models.CharField(max_length=11)  # regex: 3 nums, space, 3 nums, space, 3 nums
+    house_number = models.CharField(max_length=256, validators=[RegexValidator("^\d+\/?\d*$")])
+    zipcode = models.CharField(max_length=6, validators=[RegexValidator("^\d{3} \d{2}$")])
+    phone_number = models.CharField(max_length=11, validators=[RegexValidator("^\d{9}$")])
 
 
 
@@ -28,9 +30,10 @@ class User(AbstractUser):
 class Ticket(models.Model):
     name = models.CharField(max_length=256)
     description = models.CharField(max_length=1024)
-    state = models.CharField(max_length=256)                                      # which states? ->
-    customer_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="customer")             # on delete behaviour
-    admin_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="admin")                # on delete
+    state = models.CharField(choices=((1, "submitted"), (2, "working on"), (3, "resolved")), max_length=256, default=1)
+    creation_date_time = models.DateTimeField(default=timezone.now)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="customer")             # on delete behaviour
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name="admin")                # on delete
 
 
 # Request table
@@ -38,41 +41,42 @@ class Request(models.Model):
     description = models.CharField(max_length=256)
     estimated_time = models.FloatField()
     real_time = models.FloatField()
-    state = models.CharField(max_length=256)        # as states
-    ticket_id = models.ForeignKey(Ticket, on_delete=models.CASCADE)            # foreign key
+    state = models.CharField(choices=((1, "submitted"), (2, "working on"), (3, "resolved")), max_length=256, default=1)
+    creation_date_time = models.DateTimeField(default=timezone.now)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)            # foreign key
     technicians = models.ManyToManyField(User)        # technician-request
 
 
 # Comment-ticket - weak entity
 class TicketComment(models.Model):
-    ticket_id = models.ForeignKey(Ticket, on_delete=models.CASCADE)       # foreign key to tickets
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)       # foreign key to tickets
     # id = models.AutoField()                  # autoincrement if possible
     text = models.CharField(max_length=1024)
-    creation_date_time = models.DateTimeField()
-    author_id = models.ForeignKey(User, on_delete=models.CASCADE)           # foreign key to persons
+    creation_date_time = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)           # foreign key to persons
 
     # this should simulate weak entity
     class Meta:
-        unique_together = ('ticket_id', 'id')
+        unique_together = ('ticket', 'id')
 
 
 # Request comment table - weak entity
 class RequestComment(models.Model):
-    request_id = models.ForeignKey(Request, on_delete=models.CASCADE)
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
     # id = models.AutoField()              # autoincrement
     text = models.CharField(max_length=1024)
-    creation_date_time = models.DateTimeField()
-    author_id = models.ForeignKey(User, on_delete=models.CASCADE)       # foreign key
+    creation_date_time = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)       # foreign key
 
     class Meta:
-        unique_together = ('request_id', 'id')
+        unique_together = ('request', 'id')
 
 
 # pictures table
 class Picture(models.Model):
-    ticket_id = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
     # id = models.AutoField()             # autoincrement
-    image = models.ImageField()
+    url = models.URLField(default="")
 
     class Meta:
-        unique_together = ('ticket_id', 'id')
+        unique_together = ('ticket', 'id')
